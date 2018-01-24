b41f33dfe1214c8475759902f1126af9de384417
thiru <thirunasinan@gmail.com>
2018-01-24 16:33:35 +0530

    new, edit, update and index listing

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,7 @@
 class ArticlesController < ApplicationController
+  def index
+    @articles =Article.all    
+  end
   def new
       @article = Article.new
   end
@@ -7,8 +10,35 @@ class ArticlesController < ApplicationController
     #render plain: params[:article].inspect
 
     @article = Article.new(article_params)
-    @article.save
-    redirect_to articles_show(@article)
+
+    if @article.save
+
+      flash[:notice] = "Article Successfully created!"
+      redirect_to article_path(@article)
+      
+    else
+      render :new
+    end
+    #@article.save
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article was updated successfully!"
+      redirect_to article_path(@article)
+    else
+      render :edit
+    end
+    
   end
   private
     def article_params
